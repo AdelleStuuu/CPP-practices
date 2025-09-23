2aeb0593bd5372e093897c43cb1cf61c4c35e253
AdelleStuuu~ <adelledoesstuff@gmail.com>
2025-09-23 21:44:27 +0800

    WHAT I MADE IT WORK
    ahem, simple calculator variant 2

---
+++ PRACTICE SET 1/basic calculator variant2.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+
+using namespace std;
+
+int enterNumber()
+{
+    int number;
+    while (true)
+    {
+
+        cout << "Enter a number! ";
+        cin >> number;
+
+        if (cin.fail())
+        {
+            cin.clear();
+            cin.ignore();
+        }
+        else
+        {
+            break;
+        }
+    }
+    return number;
+}
+
+int main()
+{
+
+    int operation;
+
+    cout << " "
+         << endl;
+
+    auto num1 = enterNumber();
+    auto num2 = enterNumber();
+
+    cout << num1 << endl;
+
+    while (true)
+    {
+
+        while (true)
+        {
+            cout << "\nEnter the operation!\n1. Addition\n2. Subtraction\n3. Division\n4.Multiplication\nEnter here: ";
+            cin >> operation;
+
+            if (cin.fail())
+            {
+                cin.clear();
+                cin.ignore();
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        cout << " "
+             << endl;
+
+        if (operation == 1)
+        {
+            int sum = num1 + num2;
+            cout << num1 << " + " << num2 << " = " << sum << endl;
+            break;
+        }
+        else if (operation == 2)
+        {
+            int difference = num1 - num2;
+            cout << num1 << " - " << num2 << " = " << difference << endl;
+            break;
+        }
+        else if (operation == 3)
+        {
+            int quotient = num1 / num2;
+            cout << num1 << " / " << num2 << " = " << quotient << endl;
+            break;
+        }
+        else if (operation == 4)
+        {
+            int product = num1 * num2;
+            cout << num1 << " * " << num2 << " = " << product << endl;
+            break;
+        }
+        else
+        {
+            cout << "Invalid input btw " << endl;
+        }
+
+        cout << " "
+             << endl;
+    }
+    cout << " "
+         << endl;
+}
